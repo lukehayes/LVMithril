d4176442411a543124b4c569517ff2d07963cd37
lukehayes <lukepurplehayes@gmail.com>
2025-11-26 10:14:31 +0000

    refactor: changed font to Inter

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,8 +7,8 @@
         <title>Mithril</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+				<link rel="preconnect" href="https://fonts.bunny.net">
+				<link href="https://fonts.bunny.net/css?family=inter:300,400,700" rel="stylesheet" />
 
         <!-- Styles / Scripts -->
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
@@ -20,9 +20,6 @@
         @endif
     </head>
     <body class="">
-
         <div id="app"></div>
-
-
     </body>
 </html>
